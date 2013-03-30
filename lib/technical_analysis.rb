88f5d2842df63eb3d0219f8078a32c35bc998539
Marco Carvalho <marco.carvalho.swasthya@gmail.com>
2013-03-30 18:12:14 -0300

    added array extension

--- lib/technical_analysis.rb
+++ lib/technical_analysis.rb
@@ -1,5 +1,8 @@
 require "technical_analysis/version"
 
 module TechnicalAnalysis
+  autoload :Array, 'technical_analysis/ext/array.rb'
   # Your code goes here...
 end
+
+Array.include(TechnicalAnalysis::Array)
---
+++ lib/technical_analysis/ext/array.rb
@@ -0,0 +1,26 @@
+module TechnicalAnalysis
+  module Array
+    extend FFI::Talib
+    
+    def execute_talib(name, *params)
+      name = "ta_#{name.to_s.downcase}".to_sym
+      if FFI::Talib.implemented_talib_methods.include?(name.to_sym)
+        FFI::Talib.send(name, *params)
+      else
+        raise NotImplementedError.new("#{name} not implemented in FFI::Talib yet")
+      end
+    end
+
+    def respond_to?(method)
+      FFI::Talib.implemented_talib_methods.include?(method.to_sym) || super
+    end
+
+    def method_missing(method, *params)
+      if FFI::Talib.implemented_talib_methods.include?(method.to_sym)
+        execute_talib(method, self, *params)
+      else
+        super
+      end
+    end
+  end
+end
