8f4c6b42dbe65b613e47c66b2b95f938cd86eaf0
Marco Carvalho <marco.carvalho.swasthya@gmail.com>
2013-05-04 19:18:16 -0300

    cap modifications

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,5 @@
+require "bundler/capistrano"
+
 set :application, "technical_analysis"
 set :repository,  "git@github.com:marcocarvalho/technical_analysis.git"
 
@@ -5,11 +7,14 @@ set :scm, :git
 set :copy_exclude, [".git/*", ".svn/*", ".DS_Store"]
 ssh_options[:forward_agent] = true
 
-role :app, "digital"                          # This may be the same as your `Web` server
+role :app, "digital"
+role :db, "digital"
 
-set :deploy_via, :remote_cache
+# set :deploy_via, :remote_cache
+set :normalize_asset_timestamps, false
 set :user, "ubuntu"
+set :use_sudo, false
 
 set :branch, 'master'
-set :deploy_to, '/var/app/'
-after "deploy:update_code", "deploy:migrate"
+set :deploy_to, '/var/app/technical_analysis'
+# after "deploy:update_code", "deploy:migrate"
