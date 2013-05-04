2a34ab423794da220b3e7d6dc1ea4e69ef6d0486
Marco Carvalho <marco.carvalho.swasthya@gmail.com>
2013-05-04 18:57:46 -0300

    PAM capistrano fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,6 @@ set :copy_exclude, [".git/*", ".svn/*", ".DS_Store"]
 ssh_options[:forward_agent] = true
 
 role :app, "digital"                          # This may be the same as your `Web` server
-role :db,  "localhost", :primary => true # This is where Rails migrations will run
 
 set :deploy_via, :remote_cache
 set :user, "ubuntu"
