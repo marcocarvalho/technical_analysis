72550d7edb6de970bbc1d6c97df0a71319154cdb
Marco Carvalho <marco.carvalho.swasthya@gmail.com>
2013-05-05 18:09:26 -0300

    changes to company and symbol model

--- lib/technical_analysis/models/company.rb
+++ lib/technical_analysis/models/company.rb
@@ -3,5 +3,13 @@ class Company < ActiveRecord::Base
   has_and_belongs_to_many :portfolios
   has_and_belongs_to_many :movements
   has_and_belongs_to_many :dividends
-  has_many :symbols, :class_name => :CompanySymbol
+  has_many :company_symbols, :dependent => :destroy
+
+  def symbols
+    company_symbols.map { |s| s.symbol }
+  end
+
+  def add_symbol(str)
+    company_symbols << CompanySymbol.new(symbol: str)
+  end
 end
