dcedb84e7ac2763aaef25811b8e71b5b2b53f83a
Marco Carvalho <marco@ebehavior.com.br>
2013-05-08 13:13:53 -0300

    New Talib

--- lib/technical_analysis/ext/array.rb
+++ lib/technical_analysis/ext/array.rb
@@ -2,19 +2,19 @@ module TechnicalAnalysis
   module Array
     def execute_talib(name, *params)
       name_send = "ta_#{name.to_s.downcase}".to_sym
-      if FFI::Talib.implemented_talib_methods.include?(name.to_sym)
-        FFI::Talib.send(name_send, *params)
+      if Talib.implemented_talib_methods.include?(name.to_sym)
+        Talib.send(name_send, *params)
       else
-        raise NotImplementedError.new("#{name} not implemented in FFI::Talib yet")
+        raise NotImplementedError.new("#{name} not implemented in Talib yet")
       end
     end
 
     def respond_to?(method)
-      FFI::Talib.implemented_talib_methods.include?(method.to_sym) || super
+      Talib.implemented_talib_methods.include?(method.to_sym) || super
     end
 
     def method_missing(method, *params)
-      if FFI::Talib.implemented_talib_methods.include?(method.to_sym)
+      if Talib.implemented_talib_methods.include?(method.to_sym)
         execute_talib(method, self, *params)
       else
         super
