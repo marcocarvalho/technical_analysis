91caff0264dfa7791a8aa34fa6fde7bae9337374
Marco Carvalho <marco.carvalho.swasthya@gmail.com>
2013-05-18 14:12:01 -0300

    Fuck this shit... refactor mode on!

---
+++ lib/technical_analysis/models/signal.rb
@@ -0,0 +1,3 @@
+class Signal
+  attr_accessor :signal, :entry_point, :stop_loss, :stop_gain, :candle
+end
--- spec/technical_analysis/setup/last_maximum_disruption_spec.rb
+++ spec/technical_analysis/setup/last_maximum_disruption_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe TechnicalAnalysis::LastMaximumDisruption do
+  let(:values) { [ [10,5], [9,4], [10,5], [11,5], [12,5], [11,5], [12,5], [10,5], [9,5] ] }
+  let(:candle_array) { values.map { |m| Candle.new(high: m.first, low: m.last) } }
+  let(:entry_points) { [ Signal.new() ] }
+  subject { TechnicalAnalysis::LastMaximumDisruption.new(candle_array) }
+  it 'should return entry points' do
+    subject.run_setup.should = entry_points
+  end
 
 end
