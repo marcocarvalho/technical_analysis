0a1c42b9af9fa8ec17316c27c841c96ab20d1a38
Adilson Carvalho <adilson@adilsoncarvalho.com.br>
2013-05-18 16:27:03 -0300

    Taking the dependency of Candle off from the test

--- spec/technical_analysis/setup/last_maximum_disruption_spec.rb
+++ spec/technical_analysis/setup/last_maximum_disruption_spec.rb
@@ -1,17 +1,23 @@
 require 'spec_helper'
 
 describe TechnicalAnalysis::LastMaximumDisruption do
-  let(:c1) { Candle.new(date: '2013-01-01', high: 10, low: 5) }
-  let(:c2) { Candle.new(date: '2013-01-02', high: 11, low: 5) }
-  let(:c3) { Candle.new(date: '2013-01-03', high:  9, low: 5) }
-  let(:c4) { Candle.new(date: '2013-01-04', high: 10, low: 5) }
-  let(:c5) { Candle.new(date: '2013-01-05', high: 11, low: 5) }
-  subject { TechnicalAnalysis::LastMaximumDisruption.new([c1, c2, c3, c4, c5]) }
-  it 'should' do
-    subject.run_setup.should == [
-      { candle: c2, stop_gain: (c2.high * 1.07).round(2), stop_loss: c2.low },
-      { candle: c4, stop_gain: (c4.high * 1.07).round(2), stop_loss: c4.low },
-      { candle: c5, stop_gain: (c5.high * 1.07).round(2), stop_loss: c5.low }
-    ]
+  it 'should get signal for each entry point' do
+    candles = [1, 2, 3, 4]
+
+    candles.each do |candle|
+      # let's take the even as the selected
+      is_even_number = candle % 2 == 0
+
+      subject.should_receive(:entry_point?)
+        .with(candle)
+        .once
+        .and_return(is_even_number)
+      subject.should_receive(:signal)
+        .with(candle)
+        .once
+        .and_return(candle) if is_even_number
+    end
+
+    subject.run_setup.should == [2, 4]
   end
 end
