d8b061797d7f018ebc88f75570efb797f3e8ba8e
Marco Carvalho <marco.carvalho.swasthya@gmail.com>
2013-05-18 16:35:01 -0300

    fixed spec

--- spec/technical_analysis/setup/last_maximum_disruption_spec.rb
+++ spec/technical_analysis/setup/last_maximum_disruption_spec.rb
@@ -1,23 +1,12 @@
 require 'spec_helper'
 
 describe TechnicalAnalysis::LastMaximumDisruption do
+  subject { TechnicalAnalysis::LastMaximumDisruption.new([1,2,3,4]) }
   it 'should get signal for each entry point' do
-    candles = [1, 2, 3, 4]
+    subject.should_receive(:range).and_return([1,2,3,4])
+    subject.should_receive(:entry_point?).and_return(true, false, true, false)
+    subject.should_receive(:signal).exactly(2).times.and_return(1)
 
-    candles.each do |candle|
-      # let's take the even as the selected
-      is_even_number = candle % 2 == 0
-
-      subject.should_receive(:entry_point?)
-        .with(candle)
-        .once
-        .and_return(is_even_number)
-      subject.should_receive(:signal)
-        .with(candle)
-        .once
-        .and_return(candle) if is_even_number
-    end
-
-    subject.run_setup.should == [2, 4]
+    subject.run_setup.should == [1, 1]
   end
 end
