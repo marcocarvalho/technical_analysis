fe90d7226a6e38857858fcc529278ee62c6bf89e
Marco Carvalho <marco.carvalho.swasthya@gmail.com>
2013-06-19 08:03:45 -0300

    historical quote proxy. Missing some tests yet

---
+++ lib/technical_analysis/models/historical_quote_proxy.rb
@@ -0,0 +1,73 @@
+require 'singleton'
+
+class HistoricalQuoteProxy
+  include Singleton
+  attr_accessor :symbol_in_cache, :symbol, :start, :finish, :cache
+
+  def try_in_cache(method, method2, ret = nil)
+    if cache.respond_to?(method) and cache.send(method).respond_to?(method2)
+      cache.send(method).send(method2)
+    else
+      ret
+    end
+  end
+
+  def symbol_in_cache
+    try_in_cache(:first, :symbol, '')
+  end
+
+  def start_in_cache
+    try_in_cache(:first, :date)
+  end
+
+  def finish_in_cache
+    try_in_cache(:last, :date)
+  end
+
+  def parse_hash(hash)
+    self.symbol = hash[:symbol]
+    self.start  = parse_time(hash[:start])
+    self.finish = parse_time(hash[:finish])
+  end
+
+  def parse_time(t)
+    if t.is_a?(String)
+      Time.parse(t)
+    else
+      t
+    end
+  end
+
+  def load_quote(s, f, like = "")
+    HistoricalQuote.where("symbol = ? and date >#{like} ? and date <#{like} ?", symbol, s, f).order(:date)
+  end
+
+  def load_if_necessary
+    if cache.nil? or symbol_in_cache != symbol
+      self.cache  = load_quote(start, finish, '=')
+      self.start  = start_in_cache
+      self.finish = finish_in_cache
+    end
+
+    if start_in_cache < start
+      ret        = load_quote(start_in_cache, start)
+      self.cache = ret + cache
+    end
+
+    if finish_in_cache > finish
+      ret        = load_quote(finish, finish_in_cache)
+      self.cache = cache + ret
+    end
+  end
+
+  def where(hash)
+    parse_hash(hash)
+    load_if_necessary
+    cache
+  end
+
+  def self.where(opts)
+    hq = instance
+    hq.where(opts)
+  end
+end
---
+++ spec/technical_analysis/models/historical_quote_proxy_spec.rb
@@ -0,0 +1,103 @@
+require 'spec_helper'
+
+describe HistoricalQuoteProxy do
+  let(:start) { Candle.new }
+  let(:finish) { Candle}
+  subject { HistoricalQuoteProxy }
+
+  it 'should be a singleton' do
+    subject.ancestors.include?(:Singleton)
+  end
+
+  it '#self.where' do
+    mock = stub(:instance)
+    mock.should_receive(:where).with(:opts).and_return(:value)
+    subject.should_receive(:instance).and_return(mock)
+    subject.where(:opts).should == :value
+  end
+
+  it '#where' do
+    subject.instance.should_receive(:parse_hash).with(:opts)
+    subject.instance.should_receive(:load_if_necessary)
+    subject.instance.should_receive(:cache).and_return(:array)
+    subject.instance.where(:opts).should == :array
+  end
+
+  it '#parse_hash' do
+    subject.instance.parse_hash({ symbol: 'SYM', start: '2012-10-02', finish: Time.new(2012, 10, 3) })
+    subject.instance.symbol.should  == 'SYM'
+    subject.instance.start.should   == Time.new(2012, 10, 2)
+    subject.instance.finish.should  == Time.new(2012, 10, 3)
+  end
+
+  context 'info in cache' do
+    subject { HistoricalQuoteProxy.instance }
+    context 'no cache loaded yet' do
+      it '#symbol_in_cache' do
+        subject.symbol_in_cache.should be_empty
+      end
+
+      it '#start_in_cache' do
+        subject.start_in_cache.should be_nil
+      end
+
+      it '#finish_in_cache' do
+        subject.finish_in_cache.should be_nil
+      end
+    end
+
+    context 'cache loaded' do
+      context '#try_in_cache' do
+
+        context 'empty cache' do
+          it 'return default value if no cache' do
+            subject.try_in_cache(:test, :blah).should be_nil
+          end
+
+          it 'should return given value if no cache' do
+            subject.try_in_cache(:test, :blah, :value).should == :value
+          end
+        end
+
+        context 'with cache' do
+          let(:start_date) { Time.new(2012, 10, 1) }
+          let(:finish_date) { Time.new(2012, 10, 2) }
+          let(:symbol) { 'symbol' }
+          let(:cache) { [HistoricalQuote.new(symbol: symbol, date: start_date),
+                         HistoricalQuote.new(symbol: symbol, date: finish_date)] }
+
+          before(:each) do
+            subject.should_receive(:cache).any_number_of_times.and_return(cache)
+          end
+
+          it 'should return default value if no method exists' do
+            subject.try_in_cache(:bleh, :symbol, 'default').should == 'default'
+          end
+
+          it 'should return symbol of first element' do
+            subject.try_in_cache(:first, :symbol, 'default').should == symbol
+          end
+
+          it 'should return date of last element' do
+            subject.try_in_cache(:last, :date, 'default').should == finish_date
+          end
+        end
+      end
+
+      it '#symbol_in_cache' do
+        subject.should_receive(:try_in_cache).with(:first, :symbol, '').and_return('SYM')
+        subject.symbol_in_cache.should == 'SYM'
+      end
+
+      it '#start_in_cache' do
+        subject.should_receive(:try_in_cache).with(:first, :date).and_return(:some_date)
+        subject.start_in_cache.should == :some_date
+      end
+
+      it '#finish_in_cache' do
+        subject.should_receive(:try_in_cache).with(:last, :date).and_return(:some_other_date)
+        subject.finish_in_cache.should == :some_other_date
+      end
+    end
+  end
+end
