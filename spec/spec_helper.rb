45a29bda45d9f83771a71ec56966a29ccc1a23e9
Marco Carvalho <marco@ebehavior.com.br>
2013-07-02 13:42:39 -0300

    coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,11 @@
 require 'technical_analysis'
 
 Dir[ File.join(File.dirname(__FILE__), '**/support/*.rb' )].each { |file| require file }
+
+if(ENV["RUN_COVERAGE"])
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter "/spec/"
+  end
+  puts "Running coverage tool\n"
+end
